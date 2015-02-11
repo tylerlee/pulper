ae3bbea925bd2fb865f345be53addc20e36e9d93
tylerlee <tyler@orgsync.com>
2015-02-10 21:58:29 -0600

    spelling and style fix

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ var createTempFile = function (content, savePath, meta, req, fileName) {
 var renderPDF = function (meta, req, fileName) {
   phantom.create(function (ph) {
     ph.createPage(function (page) {
-      page.open(req.protocol + '://' + req.get('host') + '/generated/tmpFile?fileName='+fileName+'&layout=' + meta.layoutPath + '&title=' + meta.title, function (status) {
+      page.open(req.protocol + '://' + req.get('host') + '/generated/tmpFile?fileName=' + fileName + '&layout=' + meta.layoutPath + '&title=' + meta.title, function (status) {
         page.set('paperSize', {
           width: meta.pageWidth,
           height: meta.pageHeight,
